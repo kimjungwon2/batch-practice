b6e9fdae3a76d07a4500ad652b046f7f83397a86
jungwon <mac2001035@gmail.com>
2024-06-06 22:51:00 +0900

    TEST: JPA CRUD 테스트.

--- src/main/java/com/jungwon/batch/repository/packaze/PackageEntity.java
+++ src/main/java/com/jungwon/batch/repository/packaze/PackageEntity.java
@@ -1,5 +1,6 @@
 package com.jungwon.batch.repository.packaze;
 
+import com.jungwon.batch.repository.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @ToString
 @Entity
 @Table(name = "package")
-public class PackageEntity {
+public class PackageEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
